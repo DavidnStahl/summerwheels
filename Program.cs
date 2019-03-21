0787004eaa30bbcb3a6d13b8eb20206a3ca55d6f
David Ståhl <47720480+DavidnStahl@users.noreply.github.com>
2019-03-21 18:31:16 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summerwheels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a program where you enter the pattern depth on summer wheels
+            Console.Write("Here you enter the pattern depth on your summer wheels in mm ( exemple 1,2): ");
+            double patternDepth = double.Parse(Console.ReadLine());
+            if (patternDepth < 1.6)
+            {
+                Console.WriteLine("Illegal wheels!");
+            }
+            else
+            {
+                Console.WriteLine("Legal wheels!");
+            }
+            Console.ReadLine();
+
+
+            /*If the pattern depth is below 1.6mm,
+            the text "Illegal wheels!" printed, otherwise the text "Legal wheels!" printed.*/
+        }
+    }
+}
